f8c7726de6dcf30add6ee83ea4b4950f0b93c4cf
Lucasteisouza <lucasteisouza@gmail.com>
2022-11-04 22:07:16 -0300

    Requisito 12 atendido

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,72 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Favorites extends Component {
+  state = {
+    favoriteList: [],
+    loadingFavoriteList: false,
+  };
+
+  componentDidMount() {
+    this.fetchFavorites();
+  }
+
+  handleChange = ({ target }, songInfo) => {
+    const { name, checked } = target;
+    this.setState(
+      {
+        [name]: checked,
+      },
+      () => {
+        if (checked) {
+          this.addFavorite(songInfo);
+        } else {
+          this.removeFavorite(songInfo);
+        }
+      },
+    );
+  };
+
+  removeFavorite = async (songInfo) => {
+    this.setState(
+      { loadingFavoriteList: true },
+      async () => {
+        await removeSong(songInfo);
+        await this.fetchFavorites();
+      },
+    );
+  };
+
+  fetchFavorites = async () => {
+    this.setState(
+      { loadingFavoriteList: true },
+      async () => {
+        const favoriteList = await getFavoriteSongs();
+        const indexTrue = favoriteList.map(() => true);
+        this.setState({ ...indexTrue, favoriteList }, () => {
+          this.setState({ loadingFavoriteList: false });
+        });
+      },
+    );
+  };
+
   render() {
+    const { favoriteList, loadingFavoriteList } = this.state;
+    const { state } = this;
     return (
       <div data-testid="page-favorites">
         <Header />
-        <p>Favorites</p>
+        { loadingFavoriteList ? <Loading /> : favoriteList.map((e, index) => (
+          <MusicCard
+            key={ e.trackId }
+            songInfo={ e }
+            index={ index }
+            checked={ state[index] }
+            handleChange={ this.handleChange }
+          />))}
       </div>
     );
   }
